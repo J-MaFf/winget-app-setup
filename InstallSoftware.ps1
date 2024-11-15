bd2d21ec6f797f8cb04ecad18da01fb4925a8f68
Joey Maffiola <7maffiolajoey@gmail.com>
2024-11-15 11:49:59 -0600

    Initial Commit

---
+++ InstallSoftware.ps1
@@ -0,0 +1,57 @@
+# 1. Make sure the Microsoft App Installer is installed:
+#    https://www.microsoft.com/en-us/p/app-installer/9nblggh4nns1
+# 2. Edit the list of apps to install.
+# 3. Run this script as administrator.
+
+# Check if the script is run as administrator
+If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    Write-Error "You need to run this script as an administrator!"
+    Exit 1
+} else {
+    Write-Output "Running as administrator..."
+}
+
+$apps = @(
+    @{name = "7zip.7zip" },
+    @{name = "GlavSoft.TightVNC" },
+    @{name = "Adobe.Acrobat.Reader.64-bit" },
+    @{name = "Google.Chrome" },
+    @{name = "Google.Drive" },
+    @{name = "Dell.CommandUpdate.Universal" },
+    @{name = "Microsoft.PowerShell" },
+    @{name = "Microsoft.WindowsTerminal" }
+);
+
+Write-Output "Installing the following Apps:"
+ForEach ($app in $apps) {
+    Write-Output $app.name
+}
+
+$installedApps = @()
+$skippedApps = @()
+$failedApps = @()
+
+Foreach ($app in $apps) {
+    try {
+        $listApp = winget list --exact -q $app.name
+        if (![String]::Join("", $listApp).Contains($app.name)) {
+            Write-host "Installing: " $app.name
+            winget install -e -h --accept-source-agreements --accept-package-agreements --id $app.name 
+            Write-host "Successfully installed: " $app.name
+            $installedApps += $app.name
+        }
+        else {
+            Write-host "Skipping: " $app.name " (already installed)"
+            $skippedApps += $app.name
+        }
+    }
+    catch {
+        Write-Error "Failed to install: $($app.name). Error: $_"
+        $failedApps += $app.name
+    }
+}
+
+Write-Output "Summary:"
+Write-Output "Installed Apps: $($installedApps -join ', ')"
+Write-Output "Skipped Apps: $($skippedApps -join ', ')"
+Write-Output "Failed Apps: $($failedApps -join ', ')"
---
+++ Winget instructions.md
@@ -0,0 +1,17 @@
+Windows released a new command line tool in 2021 for installing software called `winget`. My verdict is: wow, it's really good! 
+
+Using this PowerShell script, within about 10 minutes it had installed over 30 applications with no interaction needed from me.
+
+Here's a quick tutorial on using `winget`.
+
+* Winget comes pre-installed on new computers, but if you don't have it, just install the [App Installer](https://www.microsoft.com/en-us/p/app-installer/9nblggh4nns1) from the Microsoft Store.
+* `winget list` shows all applications you currently have installed and labels which ones are available through winget. This is a good way to prepare your own setup script, especially if you're planning to get a new computer.
+* `winget search <name of app>` to find out if an app you want can be installed through winget.
+* `winget install` and `winget uninstall` do exactly what you think.
+
+You can install each app separately using those commands. Or if you want to use the script to automate it, here's how to do that: 
+
+1. Edit the `InstallSoftware.ps1` file to include the apps you want.
+2. Start PowerShell as administrator.
+3. If running scripts is blocked (it should be), you can temporarily unblock them with `Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope Process`.
+4. Run the script and enjoy!
