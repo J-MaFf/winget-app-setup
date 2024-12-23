cc75703dd1d8f9d9463d4bc806e50192ad506318
Joey Maffiola <7maffiolajoey@gmail.com>
2024-12-23 10:56:18 -0600

    Formated the file better.

--- InstallSoftware.ps1
+++ InstallSoftware.ps1
@@ -36,92 +36,6 @@
  Windows Terminal 
 #>
 
-# Found a bug. If the user has not installed any packages using winget, the source will not be trusted. I need
-# to add a command to trust the source. I will add a function to check if the source is trusted and if not, trust it.
-
-Param()
-
-# Check if the script is run as administrator
-If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
-    Write-Host "You need to run this script as an administrator!" -ForegroundColor Red
-    # Keep the console window open until the user presses a key
-    Write-Host "Press any key to exit..." -ForegroundColor Blue
-    [System.Console]::ReadKey($true) > $null
-    Exit 1
-} else {
-    Write-Host "Running as administrator..." -ForegroundColor Green
-}
-
-# Add the script directory to the PATH
-$scriptDirectory = Split-Path -Parent $MyInvocation.MyCommand.Definition
-Add-ToEnvironmentPath -PathToAdd $scriptDirectory -Scope 'User'
-
-$apps = @(
-    @{name = "7zip.7zip" },
-    @{name = "GlavSoft.TightVNC" },
-    @{name = "Adobe.Acrobat.Reader.64-bit" },
-    @{name = "Google.Chrome" },
-    @{name = "Google.GoogleDrive" },
-    @{name = "Dell.CommandUpdate.Universal" },
-    @{name = "Microsoft.PowerShell" },
-    @{name = "Microsoft.WindowsTerminal" }
-);
-
-Write-Host "Installing the following Apps:" -ForegroundColor Blue
-ForEach ($app in $apps) {
-    Write-Host $app.name -ForegroundColor Blue
-}
-
-$installedApps = @()
-$skippedApps = @()
-$failedApps = @()
-
-# Verify sources are trusted
-$trustedSources = @("winget", "msstore")
-ForEach ($source in $trustedSources) {
-    if (-not (Test-Source-IsTrusted -target $source)) {
-        Write-Host "Trusting source: $source" -ForegroundColor Yellow
-        Set-Sources
-    }
-}
-
-
-Foreach ($app in $apps) {
-    try {
-        $listApp = winget list --exact -q $app.name
-        if (![String]::Join("", $listApp).Contains($app.name)) {
-            Write-Host "Installing: " $app.name -ForegroundColor Blue
-            Start-Process winget -ArgumentList "install -e --accept-source-agreements --accept-package-agreements --id $($app.name)" -NoNewWindow -Wait
-            $installResult = winget list --exact -q $app.name
-            if (![String]::Join("", $installResult).Contains($app.name)) {
-                Write-Host "Failed to install: $($app.name). No package found matching input criteria." -ForegroundColor Red
-                $failedApps += $app.name
-            } else {
-                Write-Host "Successfully installed: " $app.name -ForegroundColor Green
-                $installedApps += $app.name
-            }
-        }
-        else {
-            Write-Host "Skipping: " $app.name " (already installed)" -ForegroundColor Yellow
-            $skippedApps += $app.name
-        }
-    }
-    catch {
-        Write-Host "Failed to install: $($app.name). Error: $_" -ForegroundColor Red
-        $failedApps += $app.name
-    }
-}
-
-Write-Host "Summary:" -ForegroundColor Blue
-Write-Host "Installed Apps: $($installedApps -join ', ')" -ForegroundColor Green
-Write-Host "Skipped Apps: $($skippedApps -join ', ')" -ForegroundColor Yellow
-Write-Host "Failed Apps: $($failedApps -join ', ')" -ForegroundColor Red
-
-# Keep the console window open until the user presses a key
-Write-Host "Press any key to exit..." -ForegroundColor Blue
-[System.Console]::ReadKey($true) > $null
-
-
 # ------------------------------------------------Functions------------------------------------------------
 
 <#
@@ -224,3 +138,86 @@ function Test-PathInEnvironment {
 
     return ($envPath -split ';').Contains($PathToCheck)
 }
+
+
+#------------------------------------------------Main Script------------------------------------------------
+
+# Check if the script is run as administrator
+If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    Write-Host "You need to run this script as an administrator!" -ForegroundColor Red
+    # Keep the console window open until the user presses a key
+    Write-Host "Press any key to exit..." -ForegroundColor Blue
+    [System.Console]::ReadKey($true) > $null
+    Exit 1
+} else {
+    Write-Host "Running as administrator..." -ForegroundColor Green
+}
+
+# Add the script directory to the PATH
+$scriptDirectory = Split-Path -Parent $MyInvocation.MyCommand.Definition
+Add-ToEnvironmentPath -PathToAdd $scriptDirectory -Scope 'User'
+
+$apps = @(
+    @{name = "7zip.7zip" },
+    @{name = "GlavSoft.TightVNC" },
+    @{name = "Adobe.Acrobat.Reader.64-bit" },
+    @{name = "Google.Chrome" },
+    @{name = "Google.GoogleDrive" },
+    @{name = "Dell.CommandUpdate.Universal" },
+    @{name = "Microsoft.PowerShell" },
+    @{name = "Microsoft.WindowsTerminal" }
+);
+
+Write-Host "Installing the following Apps:" -ForegroundColor Blue
+ForEach ($app in $apps) {
+    Write-Host $app.name -ForegroundColor Blue
+}
+
+$installedApps = @()
+$skippedApps = @()
+$failedApps = @()
+
+# Verify sources are trusted
+$trustedSources = @("winget", "msstore")
+ForEach ($source in $trustedSources) {
+    if (-not (Test-Source-IsTrusted -target $source)) {
+        Write-Host "Trusting source: $source" -ForegroundColor Yellow
+        Set-Source
+    }
+}
+
+
+Foreach ($app in $apps) {
+    try {
+        $listApp = winget list --exact -q $app.name
+        if (![String]::Join("", $listApp).Contains($app.name)) {
+            Write-Host "Installing: " $app.name -ForegroundColor Blue
+            Start-Process winget -ArgumentList "install -e --accept-source-agreements --accept-package-agreements --id $($app.name)" -NoNewWindow -Wait
+            $installResult = winget list --exact -q $app.name
+            if (![String]::Join("", $installResult).Contains($app.name)) {
+                Write-Host "Failed to install: $($app.name). No package found matching input criteria." -ForegroundColor Red
+                $failedApps += $app.name
+            } else {
+                Write-Host "Successfully installed: " $app.name -ForegroundColor Green
+                $installedApps += $app.name
+            }
+        }
+        else {
+            Write-Host "Skipping: " $app.name " (already installed)" -ForegroundColor Yellow
+            $skippedApps += $app.name
+        }
+    }
+    catch {
+        Write-Host "Failed to install: $($app.name). Error: $_" -ForegroundColor Red
+        $failedApps += $app.name
+    }
+}
+
+Write-Host "Summary:" -ForegroundColor Blue
+Write-Host "Installed Apps: $($installedApps -join ', ')" -ForegroundColor Green
+Write-Host "Skipped Apps: $($skippedApps -join ', ')" -ForegroundColor Yellow
+Write-Host "Failed Apps: $($failedApps -join ', ')" -ForegroundColor Red
+
+# Keep the console window open until the user presses a key
+Write-Host "Press any key to exit..." -ForegroundColor Blue
+[System.Console]::ReadKey($true) > $null
